e06acf611aa11730c54a4b9dc41e940dbde18023
frederictaillandier <frederic.taillandier@advertima.com>
2024-01-23 01:00:51 +0100

    adding gradient computing

--- src/ml.rs
+++ src/ml.rs
@@ -1,10 +1,27 @@
+use std::ops::Deref;
+
 use bevy::ecs::system::Query;
 
 use crate::{Model, Point};
 
 fn update_model(model: &mut Model) {}
 
-fn compute_cost(query: Query<&Point>, model: bevy::ecs::system::Res<crate::Model>) -> f32 {
+fn compute_gradient(query: Query<&Point>, model: &Model) -> (f32, f32) {
+    let mut w = 0.0;
+    let mut b = 0.0;
+    let m = query.iter().len() as f32;
+
+    for p in query.iter() {
+        let f_wb = model.w * p.x + model.b;
+        let d_w = (f_wb - p.y) * p.x;
+        let d_b = f_wb - p.y;
+        w += d_w;
+        b += d_b;
+    }
+    (w / m, b / m)
+}
+
+fn compute_cost(query: Query<&Point>, model: &mut Model) -> f32 {
     let mut cumulated_cost = 0.0;
     for p in query.iter() {
         let f_wb = model.w * p.x + model.b;
@@ -15,6 +32,6 @@ fn compute_cost(query: Query<&Point>, model: bevy::ecs::system::Res<crate::Model
     total_cost
 }
 
-pub fn ml(query: Query<&mut Point>, model: bevy::ecs::system::ResMut<crate::Model>) {
-    println!("ml");
+pub fn ml(query: Query<&Point>, mut model: bevy::ecs::system::ResMut<crate::Model>) {
+    let grad = compute_gradient(query, &mut model);
 }
